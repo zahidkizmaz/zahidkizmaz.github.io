0191124d9585932ba701e1a9a23429454e882088
Zahid Kizmaz <tech@zahid.rocks>
2025-10-18 22:16:26 +0200

    feat: add flake to run and develop

---
+++ flake.nix
@@ -0,0 +1,69 @@
+{
+  description = "Nix flake using flake-parts for building and running a Zola static website";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    self.submodules = true;
+  };
+
+  outputs =
+    inputs@{
+      self,
+      flake-parts,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      perSystem =
+        { pkgs, system, ... }:
+        let
+          webisePackage = pkgs.stdenv.mkDerivation rec {
+            pname = "website";
+            version = "2025-10-18";
+            src = ./.;
+            nativeBuildInputs = with pkgs; [
+              gitMinimal
+              zola
+            ];
+
+            buildPhase = ''
+              zola build
+            '';
+
+            installPhase = ''
+              mkdir -p $out/bin
+              cp -r public $out/
+              echo '#!${pkgs.runtimeShell}' > $out/bin/${pname}
+              echo "exec ${pkgs.zola}/bin/zola serve" >> $out/bin/${pname}
+              chmod +x $out/bin/${pname}
+            '';
+          };
+        in
+        {
+          packages = {
+            website = webisePackage;
+            default = webisePackage;
+          };
+
+          devShells = {
+            default = pkgs.mkShell {
+              packages = with pkgs; [
+                pre-commit
+                zola
+              ];
+              shellHook = ''
+                pre-commit install
+                git submodule update --init --recursive
+              '';
+            };
+          };
+        };
+    };
+}
